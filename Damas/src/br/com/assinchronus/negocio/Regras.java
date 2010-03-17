7ed902d4d3fbbeb653f8a8f0cbd0a469f7491278
biricius <biricius@2508d00e-4637-11de-84a6-839b6ed6c72e>
2010-03-17 03:38:56 +0000

    Ajuste da Classe Regras. Falta Damas pretas e brancas Proximo passo: Fazer metodos: Comer e Mover e classe de mensagens de erro.

--- Damas/src/br/com/assinchronus/negocio/Regras.java
+++ Damas/src/br/com/assinchronus/negocio/Regras.java
@@ -7,12 +7,121 @@ import br.com.assinchronus.componentes.Pecas;
 
 public class Regras {
 
-	public void moverPeaoBranca(Casa[][] tabuleiro, Casa casaInicial,
-			Casa casaFinal){
+	public void validar(Casa[][] tabuleiro, Casa casaInicial, Casa casaFinal) {
+
+		if ((casaInicial.getPeca() instanceof Peao)
+				&& (casaInicial.getPeca().getCor() == Pecas.BRANCA)) {
+			moverPeaoBranca(tabuleiro, casaInicial, casaFinal);
+		} else if ((casaInicial.getPeca() instanceof Peao)
+				&& (casaInicial.getPeca().getCor() == Pecas.PRETA)) {
+			moverPeaoPreta(tabuleiro, casaInicial, casaFinal);
+		} else if ((casaInicial.getPeca() instanceof Dama)
+				&& (casaInicial.getPeca().getCor() == Pecas.BRANCA)) {
+			moverDamaBranca(tabuleiro, casaInicial, casaFinal);
+		} else if ((casaInicial.getPeca() instanceof Dama)
+				&& (casaInicial.getPeca().getCor() == Pecas.PRETA)) {
+			moverDamaBranca(tabuleiro, casaInicial, casaFinal);
+		}
 	}
-	
+
+	public void moverPeaoBranca(Casa[][] tabuleiro, Casa casaInicial,
+			Casa casaFinal) {
+		if ((casaFinal.getColuna() - casaInicial.getColuna() == 1 || casaFinal
+				.getColuna()
+				- casaInicial.getColuna() == -1)
+				&& (casaFinal.getLinha() - casaInicial.getLinha() == -1))
+			if (casaFinal.getPeca() != null) {
+				// entrar na classe de erros e mandar o erro de já tem
+				// peça aki!
+			} else {
+				// chamar metodo mover -> passar casa inicial e final
+			}
+		else if ((casaFinal.getColuna() - casaInicial.getColuna()) == 2
+				&& (casaFinal.getLinha() - casaInicial.getLinha() == -2)) { // Movimento
+																			// duplo
+																			// para
+																			// a
+																			// direita
+			if (tabuleiro[casaInicial.getLinha() + 1][casaFinal.getColuna() - 1]
+					.getPeca() != null
+					&& tabuleiro[casaInicial.getLinha() + 1][casaFinal
+							.getColuna() - 1].getPeca().getCor() == Pecas.PRETA) {
+				// chamar metodo comer -> passar as 3 casas
+			} else {
+				// entrar na classe de erros e mandar o erro de
+				// movimento inválido
+			}
+		} else {
+			if ((casaFinal.getColuna() - casaInicial.getColuna()) == -2
+					&& (casaFinal.getLinha() - casaInicial.getLinha() == -2)) { // Movimento
+																				// duplo
+																				// para
+																				// a
+																				// esquerda
+				if (tabuleiro[casaInicial.getLinha() + 1][casaFinal.getColuna() + 1]
+						.getPeca() != null
+						&& tabuleiro[casaInicial.getLinha() + 1][casaFinal
+								.getColuna() + 11].getPeca().getCor() == Pecas.PRETA) {
+					// chamar metodo comer -> passar as 3 casas
+				} else {
+					// entrar na classe de erros e mandar o erro de
+					// movimento inválido
+				}
+			} else {
+				// entrar na classe de erros e mandar o erro de
+				// movimento inválido
+			}
+		}
+	}
+
 	public void moverPeaoPreta(Casa[][] tabuleiro, Casa casaInicial,
 			Casa casaFinal) {
+		if ((casaFinal.getColuna() - casaInicial.getColuna() == 1 || casaFinal
+				.getColuna()
+				- casaInicial.getColuna() == -1)
+				&& (casaFinal.getLinha() - casaInicial.getLinha() == 1))
+			if (casaFinal.getPeca() != null) {
+				// entrar na classe de erros e mandar o erro de já tem
+				// peça aki!
+			} else {
+				// chamar metodo mover -> passar casa inicial e final
+			}
+		else if ((casaFinal.getColuna() - casaInicial.getColuna()) == 2
+				&& (casaFinal.getLinha() - casaInicial.getLinha() == 2)) { // Movimento
+																			// duplo
+																			// para
+																			// a
+																			// direita
+			if (tabuleiro[casaInicial.getLinha() + 1][casaFinal.getColuna() - 1]
+					.getPeca() != null
+					&& tabuleiro[casaInicial.getLinha() + 1][casaFinal
+							.getColuna() - 1].getPeca().getCor() == Pecas.BRANCA) {
+				// chamar metodo comer -> passar as 3 casas
+			} else {
+				// entrar na classe de erros e mandar o erro de
+				// movimento inválido
+			}
+		} else {
+			if ((casaFinal.getColuna() - casaInicial.getColuna()) == -2
+					&& (casaFinal.getLinha() - casaInicial.getLinha() == 2)) { // Movimento
+																				// duplo
+																				// para
+																				// a
+																				// esquerda
+				if (tabuleiro[casaInicial.getLinha() + 1][casaFinal.getColuna() + 1]
+						.getPeca() != null
+						&& tabuleiro[casaInicial.getLinha() + 1][casaFinal
+								.getColuna() + 11].getPeca().getCor() == Pecas.BRANCA) {
+					// chamar metodo comer -> passar as 3 casas
+				} else {
+					// entrar na classe de erros e mandar o erro de
+					// movimento inválido
+				}
+			} else {
+				// entrar na classe de erros e mandar o erro de
+				// movimento inválido
+			}
+		}
 	}
 
 	public void moverDamaBranca(Casa[][] tabuleiro, Casa casaInicial,
@@ -23,102 +132,4 @@ public class Regras {
 			Casa casaFinal) {
 	}
 
-	public void validar(Casa[][] tabuleiro, Casa casaInicial, Casa casaFinal) {
-		
-	if ((casaInicial.getPeca() instanceof Peao) && (casaInicial.getPeca().getCor() == Pecas.BRANCA)) {
-			moverPeaoBranca(Casa[][] tabuleiro, Casa casaInicial, Casa casaFinal);
-		}else if ((casaInicial.getPeca() instanceof Peao) && (casaInicial.getPeca().getCor() == Pecas.PRETA)) {
-				moverPeaoPreta();
-			}else if((casaInicial.getPeca() instanceof Dama) && (casaInicial.getPeca().getCor() == Pecas.BRANCA)) {
-					moverDamaBranca();
-				}else if((casaInicial.getPeca() instanceof Dama) && (casaInicial.getPeca().getCor() == Pecas.PRETA)) {
-						moverDamaBranca();
-					}
-				}
-	
-	
-	
-	
-	
-	/*
-		if (casaInicial.getPeca() != null){
-			if (casaInicial.getPeca().getCor() == Pecas.BRANCA){
-				if ((casaFinal.getColuna() - casaInicial.getColuna() == 1 
-					|| casaFinal.getColuna() - casaInicial.getColuna() == -1)
-					&& (casaFinal.getLinha() - casaInicial.getLinha() == -1))
-					if (casaFinal.getPeca() != null) {
-						// entrar na classe de erros e mandar o erro de já tem
-						// peça aki!
-					} else {
-						// chamar metodo mover -> passar casa inicial e final
-					}
-				else if ((casaFinal.getColuna() - casaInicial.getColuna()) == 2
-						&& (casaFinal.getLinha() - casaInicial.getLinha() == -2)) { //Movimento duplo para a direita
-					if (tabuleiro[casaInicial.getLinha() + 1][casaFinal.getColuna() - 1].getPeca() != null
-						&& tabuleiro[casaInicial.getLinha() + 1][casaFinal.getColuna() - 1].getPeca().getCor() == Pecas.PRETA) {
-						// chamar metodo comer -> passar as 3 casas
-					} else {
-						// entrar na classe de erros e mandar o erro de
-						// movimento inválido
-					}
-				} else {
-					if ((casaFinal.getColuna() - casaInicial.getColuna()) == -2
-							&& (casaFinal.getLinha() - casaInicial.getLinha() == -2)) { //Movimento duplo para a esquerda
-						if (tabuleiro[casaInicial.getLinha() + 1][casaFinal.getColuna() + 1].getPeca() != null
-							&& tabuleiro[casaInicial.getLinha() + 1][casaFinal.getColuna() + 11].getPeca().getCor() == Pecas.PRETA) {
-							// chamar metodo comer -> passar as 3 casas
-						} else {
-							// entrar na classe de erros e mandar o erro de
-							// movimento inválido
-						}
-					} else {
-						// entrar na classe de erros e mandar o erro de
-						// movimento inválido
-					}
-				}
-			}
-			else	//Peca branca
-			{
-				if ((casaFinal.getColuna() - casaInicial.getColuna() == 1 
-					|| casaFinal.getColuna() - casaInicial.getColuna() == -1)
-					&& (casaFinal.getLinha() - casaInicial.getLinha() == 1))
-					if (casaFinal.getPeca() != null) {
-						// entrar na classe de erros e mandar o erro de já tem
-						// peça aki!
-					} else {
-						// chamar metodo mover -> passar casa inicial e final
-					}
-				else if ((casaFinal.getColuna() - casaInicial.getColuna()) == 2
-						&& (casaFinal.getLinha() - casaInicial.getLinha() == 2)) { //Movimento duplo para a direita
-					if (tabuleiro[casaInicial.getLinha() + 1][casaFinal.getColuna() - 1].getPeca() != null
-						&& tabuleiro[casaInicial.getLinha() + 1][casaFinal.getColuna() - 1].getPeca().getCor() == Pecas.BRANCA) {
-						// chamar metodo comer -> passar as 3 casas
-					} else {
-						// entrar na classe de erros e mandar o erro de
-						// movimento inválido
-					}
-				} else {
-					if ((casaFinal.getColuna() - casaInicial.getColuna()) == -2
-							&& (casaFinal.getLinha() - casaInicial.getLinha() == 2)) { //Movimento duplo para a esquerda
-						if (tabuleiro[casaInicial.getLinha() + 1][casaFinal.getColuna() + 1].getPeca() != null
-							&& tabuleiro[casaInicial.getLinha() + 1][casaFinal.getColuna() + 11].getPeca().getCor() == Pecas.BRANCA) {
-							// chamar metodo comer -> passar as 3 casas
-						} else {
-							// entrar na classe de erros e mandar o erro de
-							// movimento inválido
-						}
-					} else {
-						// entrar na classe de erros e mandar o erro de
-						// movimento inválido
-					}
-				}
-			}
-		}
-		else	//Nao tem peca na casa que o cara clicou
-		{
-			
-		}
-	*/
-	}
-
-
+}
