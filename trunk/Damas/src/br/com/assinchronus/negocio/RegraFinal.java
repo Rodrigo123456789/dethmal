aa6963752f14d89c88d19a4997eb7dff99eb6860
biricius <biricius@2508d00e-4637-11de-84a6-839b6ed6c72e>
2010-03-28 19:10:57 +0000

    Inicio da Classe RegraFinal para analise do tabuleiro e fim de jogo. RegraDama, atualizacao para executar metodos mover e comer Peao e Dama, atualizacao para atualizar o contador de pecas para analizar empate (GUI)

--- trunk/Damas/src/br/com/assinchronus/negocio/RegraFinal.java
+++ trunk/Damas/src/br/com/assinchronus/negocio/RegraFinal.java
@@ -1,5 +1,61 @@
 package br.com.assinchronus.negocio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.assinchronus.componentes.Casa;
+
 //erro svn
 public class RegraFinal {
-	
+
+	int qtdPeaoBranco;
+	int qtdPeaoPreto;
+	int qtdDamaBranco;
+	int qtdDamaPreto;
+
+	public List<Casa> verificaPecas(Casa[][] tabuleiro) {
+		List<Casa> casa = new ArrayList<Casa>();
+		for (int i = 0; i < tabuleiro.length; i++) {
+			for (int j = 0; j < tabuleiro[i].length; j++) {
+				if (((i % 2 == 0 && j % 2 == 0) || (i % 2 == 1 && j % 2 == 1)) && tabuleiro[i][j].getPeca() != null) {
+					casa.add(tabuleiro[i][j]); 
+					//lista de todas as casas com peca
+				}
+			}
+		}
+		return casa;
+	}
+
+	public int getQtdPeaoBranco() {
+		return qtdPeaoBranco;
+	}
+
+	public void setQtdPeaoBranco(int qtdPeaoBranco) {
+		this.qtdPeaoBranco = qtdPeaoBranco;
+	}
+
+	public int getQtdPeaoPreto() {
+		return qtdPeaoPreto;
+	}
+
+	public void setQtdPeaoPreto(int qtdPeaoPreto) {
+		this.qtdPeaoPreto = qtdPeaoPreto;
+	}
+
+	public int getQtdDamaBranco() {
+		return qtdDamaBranco;
+	}
+
+	public void setQtdDamaBranco(int qtdDamaBranco) {
+		this.qtdDamaBranco = qtdDamaBranco;
+	}
+
+	public int getQtdDamaPreto() {
+		return qtdDamaPreto;
+	}
+
+	public void setQtdDamaPreto(int qtdDamaPreto) {
+		this.qtdDamaPreto = qtdDamaPreto;
+	}
+
 }
