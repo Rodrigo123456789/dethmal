c7e93004cfb12085789d0c5881957efeb5fd23f0
biricius <biricius@2508d00e-4637-11de-84a6-839b6ed6c72e>
2010-05-08 04:27:07 +0000

    Primeira tentativa de funcionamento do Algoritmo MINIMAX com aplicação no jogo.

--- trunk/Damas/src/br/com/assinchronus/ai/Inteligencia.java
+++ trunk/Damas/src/br/com/assinchronus/ai/Inteligencia.java
@@ -3,39 +3,67 @@ package br.com.assinchronus.ai;
 import br.com.assinchronus.componentes.Tabuleiro;
 
 public class Inteligencia {
-	
+
 	private static final int NIVEL = 2;
-	
+
 	Arvore arvore;
-	
-	RegraAI regraAI; 
-	
+
+	RegraAI regraAI;
+
 	public Inteligencia(Arvore arvore) {
 		regraAI = new RegraAI(NIVEL);
 		this.arvore = arvore;
 	}
-	
-	public void jogar() {
+
+	public Tabuleiro jogar() {
 		regraAI.verificaCasas(arvore, NIVEL);
-		
-		for(Arvore arvore : this.arvore.getArvores()){
-			miniMax(arvore);
+
+		Tabuleiro tab = new Tabuleiro();
+		tab.setTabuleiro(miniMax(arvore, NIVEL).getTabuleiro());
+		return tab;
+	}
+
+	private Arvore miniMax(Arvore node, int nivel) {
+		Arvore min = new Arvore(node.getTabuleiro());
+		Arvore max = new Arvore(node.getTabuleiro());
+
+		min.setValor(350);
+		max.setValor(-350);
+
+		if (node.getArvores().isEmpty()) {
+			// nivel folha
+			return node;
+		} else {
+			for (Arvore filho : node.getArvores()) {
+				// Peca preta
+				filho.setValor(miniMax(filho, nivel - 1).getValor());
+				if (nivel % 2 == NIVEL % 2) {
+					if (max.getValor() < filho.getValor()) {
+						max = filho;
+					}
+					// Peca branca
+				} else {
+					if (min.getValor() > filho.getValor()) {
+						min = filho;
+					}
+
+				}
+			}
+
 		}
+		System.out.println(min.getValor() == 350 ? max : min);
+		return min.getValor() == 350 ? max : min;
 	}
-	
-	private Arvore miniMax(Arvore root) {
-		return null;
-	}
-	
+
 	public static void main(String[] args) {
 		long time = System.currentTimeMillis();
-		
+
 		Arvore arvore = new Arvore(new Tabuleiro().getTabuleiro());
 		Inteligencia inteligencia = new Inteligencia(arvore);
 		inteligencia.jogar();
-		
+
 		time = System.currentTimeMillis() - time;
-		
+
 		System.out.println(time);
 	}
 }
